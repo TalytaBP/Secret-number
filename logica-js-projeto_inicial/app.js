363d9fca614be6681a3fed0ee06a989222c8babf
Talyta Botelho Perrotti <talyytta@gmail.com>
2025-04-30 00:20:14 -0300

    Primeiro comimit adição de variavel e mensagem

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert ("Boas vinda ao jogo do número secreto");
+let numeroSecreto = 3;
+let escolha = prompt("Escolha um número entre 1 e 10")
+
+if (escolha == numeroSecreto) {
+    console.log("Parabéns! Você descobriu o número secreto (3)")
+}
