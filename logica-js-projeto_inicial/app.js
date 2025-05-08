88c59431a3d7b4b37aee0e015e8853447f29d806
Talyta Botelho Perrotti <talyytta@gmail.com>
2025-05-07 21:24:28 -0300

    alteração para o 2°curso em js e html

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,33 +1,9 @@
-alert ("Boas vinda ao jogo do número secreto");
-let numeroMaximo = 5000;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
-console.log(numeroSecreto);
-let escolha;
-let tentativas = 1;
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do Número Secreto";
 
-//enquanto a escolha não for igual ao número secreto
-while (escolha != numeroSecreto) {
-   escolha = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
-   //se escolha for igual ao número secreto
-    if (escolha == numeroSecreto) {
-        break;
-    } else {
-        if (escolha > numeroSecreto) {
-            alert (`O número secreto é menor que ${escolha}`);
-        } else {
-            alert (`O número secreto é maior que ${escolha}`);
-        }
-        //tentativas = tentativas + 1;
-        tentativas++;
-    }
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 a 100";
+
+function verificarNumero() {
+    console.log ("O botão foi clicado!")
 }
-
-//operador ternário
-let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-
-//if (tentativas > 1) {
-//    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-//} else {
-//    alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-//}
