3068172361460964a44083497a7747d3157fff74
TalytaBP <talyytta@gmail.com>
2025-05-23 12:59:59 -0300

    Modificação de h1 e p, adição de contator de tentativas e limpeza do campo automática

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -5,6 +5,7 @@
 // paragrafo.innerHTML = "Escolha um número entre 1 a 100";
 
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -12,13 +13,35 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 exibirTextoNaTela("h1", "Jogo do Número Secreto");
-exibirTextoNaTela("p", "Escolha um número entre 1 a 100");
+exibirTextoNaTela("p", "Escolha um número entre 1 a 10");
  
 function verificarNumero() {
     let selecionar = document.querySelector("input").value;
+
+    if (selecionar == numeroSecreto) {
+        exibirTextoNaTela("h1", "Você acertou!");
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela("p", mensagemTentativas);
+    } else {
+        if (selecionar > numeroSecreto) {
+            exibirTextoNaTela ("p", "O número secreto é menor");
+        } else {
+            exibirTextoNaTela ("p", "O número secreto é maior");
+        }
+    }
+    //tentativas = tentativas + 1;
+    tentativas++;
+    limparCampo();
+
     console.log (selecionar == numeroSecreto);
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 100 +1);
+    return parseInt(Math.random() * 10 +1);
+}
+
+function limparCampo() {
+    selecionar = document.querySelector("input");
+    selecionar.value = " ";
 }
